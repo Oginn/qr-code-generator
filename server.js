33c78bbf2053b145158b63074978db19a1cb8c80
Oginn Adeniyi <oginniadeniyi@gmail.com>
2025-03-19 14:31:49 +0100

    Updated files and fixed structure

--- server.js
+++ server.js
@@ -1,51 +1,34 @@
+import express from 'express';
 import inquirer from 'inquirer';
 import qr from 'qr-image';
 import fs from 'fs';
+import path from 'path';
 
-inquirer
-  .prompt([
-    {
-      type: 'list',
-      name: 'type',
-      message: 'What do you want to encode?',
-      choices: ['URL', 'Text', 'Phone Number'],
-    },
-    {
-      type: 'input',
-      name: 'data',
-      message: (answers) => {
-        switch (answers.type) {
-          case 'URL':
-            return 'Enter the URL:';
-          case 'Text':
-            return 'Enter the text:';
-          case 'Phone Number':
-            return 'Enter the phone number:';
-          default:
-            return 'Enter the data:';
+const app = express();
+const PORT = 3000;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static('.'));
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(process.cwd(), 'index.html'));
+});
+
+app.post('/generate', (req, res) => {
+    const url = req.body.url;
+    const qr_svg = qr.image(url, { type: 'png' });
+    const qrPath = 'qr_image.png';
+
+    qr_svg.pipe(fs.createWriteStream(qrPath));
+    fs.writeFile('data.txt', url, (err) => {
+        if (err) {
+            res.status(500).send('Error saving data');
+        } else {
+            res.send('QR Code Generated');
         }
-      },
-    },
-  ])
-  .then((answers) => {
-    const { type, data } = answers;
-
-    // Generate QR code
-    const qr_svg = qr.image(data, { type: 'png' });
-    qr_svg.pipe(fs.createWriteStream('qr_image.png'));
-
-    // Save the data to a file
-    fs.writeFile('data.txt', data, (err) => {
-      if (err) throw err;
-      console.log('The file has been saved!');
     });
+});
 
-    console.log(`QR code generated for ${type}: ${data}`);
-  })
-  .catch((error) => {
-    if (error.isTtyError) {
-      console.error('Prompt could not be rendered in the current environment.');
-    } else {
-      console.error('Something went wrong:', error);
-    }
-  });
+app.listen(PORT, () => {
+    console.log(`Server running at http://localhost:${PORT}`);
+});
